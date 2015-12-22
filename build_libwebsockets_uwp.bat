7bb63ff23a2589d416d0184105d5c32a28957a09
DannyHe <dannyjiajia@gmail.com>
2015-12-22 11:35:12 +0800

    support vs2015 for uwp project

--- build_libwebsockets_uwp.bat
+++ build_libwebsockets_uwp.bat
@@ -3,27 +3,22 @@ set ARGS=-DLWS_WITHOUT_TEST_SERVER:BOOL="1" -DLWS_IPV6:BOOL="0" -DLWS_WITHOUT_TE
 
 mkdir bin
 cd bin
-mkdir WindowsPhone_8.1
-cd WindowsPhone_8.1
+mkdir UWP
+cd UWP
 mkdir arm
 mkdir win32
 cd arm
-cmake -G"Visual Studio 12 2013 ARM" -DCMAKE_SYSTEM_NAME=WindowsPhone -DCMAKE_SYSTEM_VERSION=8.1 %ARGS% ../../../
+cmake -G"Visual Studio 14 2015 ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 %ARGS% ../../../
 cd ..\
 cd win32
-cmake -G"Visual Studio 12 2013" -DCMAKE_SYSTEM_NAME=WindowsPhone -DCMAKE_SYSTEM_VERSION=8.1  %ARGS%  ../../../
+cmake -G"Visual Studio 14 2015" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0  %ARGS%  ../../../
 cd ..\..\..\
-
-echo./*
-echo. * Check VC++ environment...
-echo. */
-echo.
 
 set FOUND_VC=0
 
-if defined VS120COMNTOOLS (
-    set VSTOOLS="%VS120COMNTOOLS%"
-    set VC_VER=120
+if defined VS140COMNTOOLS (
+    set VSTOOLS="%VS140COMNTOOLS%"
+    set VC_VER=140
     set FOUND_VC=1
 ) 
 
@@ -39,15 +34,15 @@ if not defined VSVARS (
 
 
 echo./*
-echo. * Building libwebsockets for windows phone 8.1..
+echo. * Building libwebsockets for uwp..
 echo. */
 echo.
 
 
 call %VSVARS%
 if %FOUND_VC%==1 (
-msbuild  bin\WindowsPhone_8.1\win32\libwebsockets.sln /p:Configuration="MinSizeRel"  /p:Platform="Win32"
-msbuild  bin\WindowsPhone_8.1\arm\libwebsockets.sln /p:Configuration="MinSizeRel"  /p:Platform="ARM"
+msbuild  bin\UWP\arm\libwebsockets.sln /p:Configuration="MinSizeRel"  /p:Platform="ARM"
+msbuild  bin\UWP\win32\libwebsockets.sln /p:Configuration="MinSizeRel"  /p:Platform="Win32"
 ) else (
     echo Script error.
     goto ERROR
